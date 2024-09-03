4823f6464b14ffdbe2097474e8f8697b9742c210
Marc <kastholm1995@gmail.com>
2024-09-03 12:20:02 +0200

    middleware loading fix

--- APP/middleware.ts
+++ APP/middleware.ts
@@ -1,30 +1,37 @@
 import { NextResponse, NextRequest } from 'next/server';
-import { getData } from './app/api/data/GetData';
+import { getMiddlewareData } from './app/api/data/GetData';
 import { singleArticle } from './app/models/singleArticle';
 
 export async function middleware(req: NextRequest) {
     const url = req.nextUrl;
-    const slug = url.pathname.split('/').pop();
+    const slug: string | undefined = url.pathname.split('/').pop();
 
     // Check for the 'redirected' query parameter to prevent redirect loop
     if (url.searchParams.has('redirected')) {
         return NextResponse.next();
     }
 
-  const { singleArticle: data } = await getData(slug) as { singleArticle: singleArticle[]};
-  const article = data[0] as singleArticle;
+    console.log(`Checking for redirect for slug: ${slug}`);
 
-  //console.log(article, 'data MIDDLE', data[0], 'sent to article page MIDDLE', slug);
+    // Fetch the relevant articles
+    const data = await getMiddlewareData() as singleArticle[];
+
+    console.log(`Data: ${JSON.stringify(data)}`);
+
+    // Find the article by newSlug, articleSlug, or oldSlugs
+    const article = 
+    data.find(({ articleSlug }) => articleSlug === slug) ||
+    data.find(({ newSlug }) => newSlug === slug) ||
+    data.find(({ oldSlugs }) => oldSlugs && oldSlugs.includes(slug));
 
     if (article && article.newSlug) {
-        //console.log(`Redirecting to new slug: ${article.newSlug}`);
-
+        // Redirect to the new slug
         const redirectUrl = new URL(`/artikel/${article.newSlug}`, req.url);
-        redirectUrl.searchParams.set('redirected', 'true');  // Add query parameter to indicate redirect
-
+        redirectUrl.searchParams.set('redirected', 'true');  // Add query parameter to prevent loop
         return NextResponse.redirect(redirectUrl, 301);
     } else {
-        //console.log(`No redirect needed for slug: ${slug}`);
+        console.log(`No redirect needed for slug: ${slug}`);
+        NextResponse.next();
     }
 
     return NextResponse.next();
@@ -33,5 +40,3 @@ export async function middleware(req: NextRequest) {
 export const config = {
     matcher: '/artikel/:path*',
 };
-
-
