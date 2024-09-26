1efed79fe480baa081c9a07d2ce9d87944ccd7e6
Marc <kastholm1995@gmail.com>
2024-09-26 09:45:29 +0200

    uri fix

--- APP/middleware.ts
+++ APP/middleware.ts
@@ -1,4 +1,4 @@
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server'; 
 import { getMiddlewareData } from './api/getMiddlewareData';
 import { singleArticle } from './app/(home)/(pages)/artikel/models/singleArticle';
 import theme from './app/lib/theme.json';
@@ -9,24 +9,22 @@ export async function middleware(req: NextRequest) {
     const fullPath: string = url.pathname;
 
     const today = new Date();
-const formattedDate = ('0' + (today.getMonth() + 1)).slice(-2) + '/' +
-                      ('0' + today.getDate()).slice(-2) + '/' +
-                      today.getFullYear().toString().slice(-2);
-const encodedDate = encodeURIComponent(formattedDate);
+    const formattedDate = ('0' + (today.getMonth() + 1)).slice(-2) + '/' +
+                          ('0' + today.getDate()).slice(-2) + '/' +
+                          today.getFullYear().toString().slice(-2);
 
+   
+    const encodedDate = formattedDate.replace(/\//g, '%2F');
 
     if (url.searchParams.has('d')) {
         return NextResponse.next();
     }
 
-
     // Fetch the relevant articles
     const data = await getMiddlewareData(slug) as singleArticle[];
-    
-    
 
     if (data) {
-        // Redirect to the new slug
+        // Redirect til den nye slug med dato som søgeparameter
         const redirectUrl = new URL(`${theme.site_url}/artikel/${data.newSlug}`, req.url);
         redirectUrl.searchParams.set('d', `${encodedDate}`);
         return NextResponse.redirect(redirectUrl, 301);
