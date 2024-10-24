9f457ad725f800e3e6f155b389e9fc43391a7a34
Marc <kastholm1995@gmail.com>
2024-10-24 13:54:18 +0200

    showheroes revert

--- APP/app/(home)/components/AdScripts/LoadShowHeroes.tsx
+++ APP/app/(home)/components/AdScripts/LoadShowHeroes.tsx
@@ -1,22 +1,21 @@
-'use client'
-import Script from 'next/script';
+import React from 'react';
 
 export default function LoadShowHeroes() {
   return (
-    <>
-      {/* Placeholder for the Viralize widget */}
-      <aside>
-        <div id="viralizeContainer"></div>
-      </aside>
-
-      {/* Load the Viralize script */}
-      <Script
-        id="viralize-script"
-        src="https://content.viralize.tv/display/?zid=AAFp6TIrtjcx6N9Y"
-        type="text/javascript"
-        data-wid="viralizeContainer"
-        strategy="beforeInteractive"
+    <aside>
+      <div id="viralizeContainer"></div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+            <script
+              src="https://content.viralize.tv/display/?zid=AAFp6TIrtjcx6N9Y"
+              data-wid="viralizeContainer"
+              type="text/javascript"
+            ></script>
+          `,
+        }}
       />
-    </>
+    </aside>
   );
 }
+
