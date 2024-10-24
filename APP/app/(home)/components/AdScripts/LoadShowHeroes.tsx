7bd00b802a56ac4b9b8fff547d62b788c55fe065
Marc <kastholm1995@gmail.com>
2024-10-24 13:51:31 +0200

    showheroes test

--- APP/app/(home)/components/AdScripts/LoadShowHeroes.tsx
+++ APP/app/(home)/components/AdScripts/LoadShowHeroes.tsx
@@ -1,29 +1,22 @@
 'use client'
 import Script from 'next/script';
-import { useEffect } from 'react';
 
 export default function LoadShowHeroes() {
-  useEffect(() => {
-    // Check if the Viralize script has already been loaded
-    if (!document.getElementById('viralize-script')) {
-      // Create a script element
-      const script = document.createElement('script');
-      script.id = 'viralize-script';
-      script.type = 'text/javascript';
-      script.src = 'https://content.viralize.tv/display/?zid=AAFp6TIrtjcx6N9Y';
-      script.setAttribute('data-wid', 'viralizeContainer');
-
-      // Append the script to the document head or body
-      document.body.appendChild(script);
-    }
-  }, []);
-
   return (
     <>
       {/* Placeholder for the Viralize widget */}
       <aside>
         <div id="viralizeContainer"></div>
       </aside>
+
+      {/* Load the Viralize script */}
+      <Script
+        id="viralize-script"
+        src="https://content.viralize.tv/display/?zid=AAFp6TIrtjcx6N9Y"
+        type="text/javascript"
+        data-wid="viralizeContainer"
+        strategy="beforeInteractive"
+      />
     </>
   );
 }
