bf0850df6f7080b363619deca2ba2532505ae0ec
Kastholm <marc@aguardio.com>
2024-11-13 21:41:50 +0100

    testing ads

--- APP/app/(home)/components/AdScripts/LoadAds.tsx
+++ APP/app/(home)/components/AdScripts/LoadAds.tsx
@@ -1,12 +1,11 @@
-'use client';
+"use client";
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 
-export default function LoadAds() { 
+export default function LoadAds() {
   const pathname = usePathname();
 
   useEffect(() => {
-
     const scriptUrls = [
       "https://securepubads.g.doubleclick.net/tag/js/gpt.js",
       /* "https://mgdk-cdn.relevant-digital.com/static/tags/66bdb1b086834271b536bf67.js", */
@@ -15,29 +14,30 @@ export default function LoadAds() {
 
     const loadScript = (src: string) => {
       return new Promise((resolve, reject) => {
-        const script = document.createElement('script')
-        script.src = src
-        script.async = true
-        script.onload = resolve
-        script.onerror = reject
-        document.body.appendChild(script)
+        const script = document.createElement("script");
+        script.src = src;
+        script.async = true;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.body.appendChild(script);
+      });
+    };
+
+    loadScript("https://securepubads.g.doubleclick.net/tag/js/gpt.js")
+      .then(() => {
+        return Promise.all(
+          scriptUrls
+            .filter(
+              (url) =>
+                url !== "https://securepubads.g.doubleclick.net/tag/js/gpt.js"
+            )
+            .map((url) => loadScript(url))
+        );
       })
-    }
-
-    loadScript('/lib/ad-script.min.js')
-    .then(() => {
-      return Promise.all(
-        scriptUrls
-        .filter((url) => url !== 'https://mgdk-cdn.relevant-digital.com/static/tags/66bdb1b086834271b536bf67.js')
-        .map((url) => loadScript(url))
-      )
-    })
-    .catch((error) => {
-      console.error('Error loading scripts:', error)
-    })
-
-  }, [pathname])
+      .catch((error) => {
+        console.error("Error loading scripts:", error);
+      });
+  }, [pathname]);
 
   return null;
-
 }
--- APP/app/(home)/layout.tsx
+++ APP/app/(home)/layout.tsx
@@ -20,38 +20,56 @@ import NewsletterPopup from "./components/MailChimp/NewsletterPopUp";
 
 export const revalidate = 600;
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter', display: 'swap', });
-const mulish = Mulish({ subsets: ["latin"], weight: ["600", "700", "900"], variable: '--font-mulish', display: 'swap', });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+const mulish = Mulish({
+  subsets: ["latin"],
+  weight: ["600", "700", "900"],
+  variable: "--font-mulish",
+  display: "swap",
+});
 
-
-
-const InfiniteScroll = ({ data } : {data : ArticleModel[]}) => {
+const InfiniteScroll = ({ data }: { data: ArticleModel[] }) => {
   return (
     <section className="grid grid-cols-[1fr_auto] md:gap-8 rounded-xl bg-second_color_light dark:bg-second_color_dark">
       <SubArticlesInfiniteScroll data={data} startIndex={7} endIndex={30} />
       <div className="!sticky top-20 mt-2 h-[80vh] hidden max-w-[320px] lg:inline-block">
-        <aside className="desktop hidden md:block" data-ad-unit-id={`/${theme.site_ad_id}/${theme.site_ad_name}/Square_2`}></aside>
-        <TrendingArticlesList data={data} dayInterval={14} startIndex={0} endIndex={30} articleAmount={6} />
+        <aside
+          className="desktop hidden md:block"
+          data-ad-unit-id={`/${theme.site_ad_id}/${theme.site_ad_name}/Square_2`}
+        ></aside>
+        <TrendingArticlesList
+          data={data}
+          dayInterval={14}
+          startIndex={0}
+          endIndex={30}
+          articleAmount={6}
+        />
       </div>
     </section>
   );
 };
 
 // Dynamisk komponent
-const DynamicArticles = dynamic<React.ComponentType<{ data: ArticleModel[] }>>(() => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(InfiniteScroll), 2000); 
-  });
-}, {
-  loading: () => null, 
-});
-
+const DynamicArticles = dynamic<React.ComponentType<{ data: ArticleModel[] }>>(
+  () => {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(InfiniteScroll), 2000);
+    });
+  },
+  {
+    loading: () => null,
+  }
+);
 
 /* -------------------------------------------------------------------------- */
 /*                                   CONTENT                                  */
 /* -------------------------------------------------------------------------- */
 export default async function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
@@ -64,26 +82,27 @@ export default async function RootLayout({
         href="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
         as="script"
       />
-      {/* <link rel="dns-prefetch" href="//adx.adform.net" crossOrigin="" />
-      <link rel="dns-prefetch" href="//ads.pubmatic.com" crossOrigin="" /> */}
-      <link rel="alternate" type="application/rss+xml" href={`${theme.feed_url}`} />
-      <body className={`${inter.variable} ${mulish.variable}`}>
-      <script
-        async
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      <link rel="dns-prefetch" href="//adx.adform.net" crossOrigin="" />
+      <link rel="dns-prefetch" href="//ads.pubmatic.com" crossOrigin="" />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        href={`${theme.feed_url}`}
       />
-       
-       {/*  <AdBlockDetect /> */}
+      <body className={`${inter.variable} ${mulish.variable}`}>
+        <script
+          async
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+
+        {/*  <AdBlockDetect /> */}
         {/* <NewsletterPopup /> */}
         <Header />
         <main className="bg-second_color_light dark:bg-main_color_dark">
-          {children} 
+          {children}
 
-
-        {/* <DynamicArticles data={data} /> */}
-
-        
+          {/* <DynamicArticles data={data} /> */}
         </main>
         <GoogleAnalyticsScripts />
         <Footer />
@@ -101,9 +120,7 @@ export default async function RootLayout({
           `,
         }}
       /> */}
-      <LoadAds />
-
-      
+        <LoadAds />
       </body>
     </html>
   );
