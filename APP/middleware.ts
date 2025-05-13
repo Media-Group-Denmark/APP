b82a05d5c013b16c431c0c99ad9c9e0a8e0ff356
Marc MGDK <mac@mgdk.dk>
2025-05-13 14:36:19 +0200

    revert to fix whitescreen bug

--- APP/middleware.ts
+++ APP/middleware.ts
@@ -5,42 +5,26 @@ import theme from "./app/lib/theme.json";
 
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
-  const hostname = req.headers.get("host");
+  const hostname = req.headers.get('host');
+  
+  const oldDomain = 'xn--pengehjrnet-mgb.dk'; 
+  const newDomain = 'pengehjoernet.dk';
 
-  const oldDomain = "xn--pengehjrnet-mgb.dk";
-  const newDomain = "pengehjoernet.dk";
-
-  // Redirect fra gammelt domæne
   if (hostname === oldDomain) {
-    url.hostname = newDomain;
-    return NextResponse.redirect(url, 301);
-  }
-
-  let modified = false;
-
-  url.searchParams.forEach((_, key) => {
-    if (["p", "redirected"].includes(key)) {
-      url.searchParams.delete(key);
-      modified = true;
+      url.hostname = newDomain;
+      return NextResponse.redirect(url, 301);
     }
-  });
-  
-  // Special case for _rsc – den skjules i nogle setups
-  if (url.search.includes("_rsc")) {
-    url.searchParams.delete("_rsc");
-    modified = true;
-  }
-  
-  if (modified) {
-    return NextResponse.redirect(url, 301);
-  }
-
-  // Slug-redirect
+    
   const slug: string | undefined = url.pathname.split("/").pop();
+
   const data = await getMiddlewareData(slug) as singleArticle;
 
   if (data?.newSlug && data.newSlug !== slug) {
-    const redirectUrl = new URL(`${theme.site_url}/artikel/${data.newSlug}`, req.url);
+    // Redirect to the new slug
+    const redirectUrl = new URL(
+      `${theme.site_url}/artikel/${data.newSlug}`,
+      req.url
+    );
     return NextResponse.redirect(redirectUrl, 301);
   }
 
